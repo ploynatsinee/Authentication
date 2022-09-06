3f767caef3ee626b430eb8df6ed85fe6d5f64628
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-06 14:27:49 +0700

    format doc

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,24 +4,24 @@ const { v4: uuidv4 } = require('uuid');
 const createUser = async (req, res) => {
     // res.send('POST API');
     try {
-    const newUser = new User({ user_id: uuidv4(), ...req.body });
-      await newUser.save();
-      res.send("new user create success");
+        const newUser = new User({ user_id: uuidv4(), ...req.body });
+        await newUser.save();
+        res.send("new user create success");
     } catch (error) {
-      res.status(400).send(error);
-      console.log(error)
+        res.status(400).send(error);
+        console.log(error)
     }
-   
-  };
-  
-  const getUserAll = async (req, res, next) => {
+
+};
+
+const getUserAll = async (req, res, next) => {
     console.log("Hello")
     const users = await User.find()
     console.log(users)
-    res.send(users.map((users)=> users.toJSON()))
-  };
-  
-  module.exports = {
+    res.send(users.map((users) => users.toJSON()))
+};
+
+module.exports = {
     createUser,
     getUserAll,
-  };
+};
