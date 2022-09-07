9df7a3ac999ad077908423e4cca5dc22af772201
Natsinee Tanapornrat <101309777+ploynatsinee@users.noreply.github.com>
2022-09-07 16:48:57 +0700

    space

--- routers/userRoute.js
+++ routers/userRoute.js
@@ -6,4 +6,5 @@ userRoutes.post("/signup", userController.createUser);
 userRoutes.post("/signin",userController.signIn)
 userRoutes.post("/signout",userController.signOut)
 userRoutes.get("/",userController.getUserAll)
+
 module.exports = userRoutes;
